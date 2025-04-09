b6539048d7733baa5fd47fbe7dca81f216d6bcd7
SENA <SENA@ARMSCCCCTGOD211.FormacionSena.RED>
2025-04-09 12:48:03 -0500

    front-end v 0.0.1

---
+++ botdt/src/assets/UI/header.jsx
@@ -0,0 +1,22 @@
+import 
+
+function Header() {
+
+  return (
+    <>
+      
+      <div className="">
+        <img src="" alt="" />
+        <div>
+            <input type="Inicio" value="" />
+            <input type="Noticias" value="" />
+            <input type="button" value="" />
+            <input type="Iniciar session" value="" />
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default Header
