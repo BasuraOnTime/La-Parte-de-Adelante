db776efd6a5b9cb08a75bc5af9d0d84eb2efe7d5
SENA <SENA@ARMSCCCCTGOD211.FormacionSena.RED>
2025-05-06 16:19:28 -0500

    update register new model white color body

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Main from '../../Layouts/Header/Main/Main';
+import Header from '../../Layouts/Header/Header';
 export default function Home() {
     return (
      <>
+        
         <Main/>
      </>
     );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,30 +1,29 @@
 import Logo from '../../UI/logo/Logo';
+import './Register.css';
 import GoogleLoginButton from '../../UI/botonG/botonG';
 import Divider from '../../UI/Divider/Divider';
 import CreateAccountForm from '../../UI/CrearCuenta/CrearCuenta';
 import Footer from '../../UI/Footer/Footer';
 import Correo from '../../UI/CorreoImput/Correo';
+import DividerB from '../../UI/dividerB/DividerB'
 
 const XLanding = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-between bg-[rgb(10,196,150)] text-black">
+    <div className=" flex flex-col justify-between bg-[rgb(255,255,255)] text-black">
       <div className="flex flex-1">
         <Logo />
 
-        <div className="w-1/2 flex flex-col justify-center px-8">
-          <h1 className="text-5xl font-bold mb-12 leading-tight">
-            Lo que está <br /> pasando ahora
+        <div id='Box-R' className="Register w-1/3  h-3/4 p-6 top-1/5 sticky flex flex-col justify-center px-8 "  >
+          <h1 className="text-5xl text-center font-bold mb-12 leading-tight">
+            Bienvenido a un lugar <br /> mas limpio 
           </h1>
-
-          <h2 className="text-2xl font-bold mb-6">Únete Hoy</h2>
-          <Correo />
-          <div className="grid  pl-20 pr-20 grid-cols-3 items-center m-2 max-w-lvh">
-            <div className="h-[1px] bg-black w-full" />
-            <div className="h-[9px] w-[9px]   border-2 border-b-black rounded-full mx-auto" />
-            <div className="h-[1px] bg-black w-full" />
+          <DividerB/> 
+          <div className='flex flex-col justify-center items-center'>
+          <input className='rounded-xl bg-[var(--Vclaro2)] w-100 h-9 text-center placeholder:text-center  bg-white' type="text" placeholder='Correo' />
+          <br/>
+          <input className='rounded-xl bg-[var(--Vclaro2)] w-100 h-9 text-center placeholder:text-center  bg-white' type="password" placeholder='Contraseña' />
           </div>
-
-          <Correo />
+          <DividerB/>
           <GoogleLoginButton />
           <Divider />
           <CreateAccountForm />
@@ -32,7 +31,7 @@ const XLanding = () => {
         </div>
       </div>
       
-      <Footer />
+     
     </div>
     
   );
