30a0f938a3c6bc235f7f97ae31de93155b8545d1
SENA <SENA@ARMSCCCCTGOD211.FormacionSena.RED>
2025-05-15 13:12:42 -0500

    mod header

--- src/UI/BotonBack/BotonBack.jsx
+++ src/UI/BotonBack/BotonBack.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import  '../BotonBack/BotonBack.css';	
 export const ItemNavBar = ({ content = 'Go Back', route = '/', icon }) => {
   return (
-    <div className="fixed top-4 left-4 z-50">
-      <NavLink to={route}>
-        <button
-          id="botones"
-          className="boton1 shadow-2xl shadow-[#000000a9] border-t-2 bg-white border-b-3 border-l-3 border-r-3 w-14 h-18"
-          title={content}
-          aria-label={content}
-        >
-          <div className="icon-c bg-white w-10 h-10 border-2 rounded-full justify-center items-center flex mx-auto">
-            <img className="main" src={icon} alt={content} id="icon" />
-          </div>
-        </button>
-      </NavLink>
+   <div class="button-container">
+  <button class="brutalist-button openai">
+    <div class="openai-logo">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="openai-icon"
+      >
+        <path
+          fill="#10A37F"><h1></h1></path>
+      </svg>
     </div>
+    <div class="button-text">
+     
+    </div>
+  </button>
+</div>
   );
 };
 
