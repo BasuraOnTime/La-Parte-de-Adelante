de855a3060aae6418063808e46c4b6f2fd4e3d24
David Muñoz <davidmunozmoreno2@gmail.com>
2025-05-29 09:51:06 -0500

    feat: se agrego el apartado de inicio de seccion del admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import logoBasuraOnTime from '../../assets/img/icons/logoBasuraOnTime.png'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import Swal from 'sweetalert2'
+import axios from 'axios'
 import "./Admin.css"
 
 const Admin = () => {
-
+  
   const navigate = useNavigate();
-  const handleLogin = () => {
-    
-    const user = document.querySelector('input[placeholder="Usuario"]').value;
-    const password = document.querySelector('input[placeholder="Contraseña"]').value;
-    
+  const URL = 'http://localhost:10101/authAdmin';
+  let sucess = false;   
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleLogin = async() => {
+    try{
+      const response = await axios.post(URL, {
+        email,
+        password
+      })
+      sucess = true;
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      handleAlert();
+    } catch (error) {
+      sucess = false;
+      handleAlert();
+      console.error('Error al iniciar sesión:', error);
+    }
+  }
+
+  const handleAlert = () => {
     Swal.fire({
           title: 'Procesando...',
           text: 'Estamos procesando tu solicitud',
@@ -26,7 +53,7 @@ const Admin = () => {
     
     setTimeout(() => {
       
-    if (user === "admin" && password === "admin") {
+    if (sucess) {
       Swal.fire({
         title: 'Bienvenido',
         text: 'Has iniciado sesión correctamente',
@@ -65,8 +92,8 @@ const Admin = () => {
     </div>
     <div className='FontGeologica flex flex-col justify-center items-center gap-3.5 bg-[var(--Voscuro2)] w-120 h-100 rounded-4xl'>
         <p className='FontCursive text-5xl p-7 text-white'> Administrador</p>
-        <input type="text" placeholder='Usuario' className='rounded-md bg-[var(--Vclaro2)] w-100 h-10 text-center placeholder:text-center text-white' />
-        <input type="text" placeholder='Contraseña' className='rounded-md bg-[var(--Vclaro2)] w-100 h-10 text-center placeholder:text-center text-white' />
+        <input onChange={handleEmailChange} type="text" placeholder='Correo' className='rounded-md bg-[var(--Vclaro2)] w-100 h-10 text-center placeholder:text-center text-white' />
+        <input onChange={handlePasswordChange} type="text" placeholder='Contraseña' className='rounded-md bg-[var(--Vclaro2)] w-100 h-10 text-center placeholder:text-center text-white' />
         <button className='rounded-md w-100 h-10 bg-[var(--Vclaro)] text-white group cursor-pointer transition-all duration-300 ease-in-out
                                             hover:scale-105 hover:shadow-2xl hover:bg-opacity-90 active:scale-95' onClick={handleLogin}> iniciar sesion </button>
     </div>
